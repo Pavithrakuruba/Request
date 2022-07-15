0183e86c1b13670d441a5bbd702a2b65a40dc74a
Pavithrakuruba <103957063+Pavithrakuruba@users.noreply.github.com>
2022-07-15 18:32:38 +0530

    Add files via upload

---
+++ 1question.py
@@ -0,0 +1,38 @@
+# import requests
+# import json
+# saral=requests=get.("http://saral.navgurukul.org/api/courses")
+# data1=saral.jsal()
+# with open("data.jsan","w")as file:
+#     json.dump(data1,file,indent=2)
+# serial_no=1
+# for indedex1 in 
+# data1["availablecourse"]:
+# print(serial_no,index1["name"],index1["id"])
+# serial_no+=1
+# topic=int(input("WHICH TOPIC DO YOU WANT??"))
+# print(data["availalecourses"[topic-1]["name"]])
+
+
+
+# temp = 10 	 # global-scope variable
+
+# def func():
+#       temp = 20   # local-scope variable
+#       print(temp)
+
+# print(temp) 	 # output => 10
+# func() 		 # output => 20
+# print(temp) 	 # output => 
+
+
+
+
+# my_list = [[10,20,30],[40,50,60],[70,80,90]]
+
+# flattened = [x for temp in my_list for x in temp]
+# # output => [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# print(flattened)
+
+
+
+# my_list=[[12,3,45,8,9,7,]]
